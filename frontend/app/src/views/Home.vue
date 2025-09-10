3a507035fe58d15db52515eab87fa96886b03ae3
CNyuchenZhang <1029238540@qq.com>
2025-09-10 17:02:58 +0800

    build docker-compose dockerfile nginx and some basic frontend and backend frame

---
+++ frontend/app/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <h1>Welcome to the Volunteer-up Platform</h1>
+    <p>This is a platform connecting Non-Profit Organizations, Visitors, and Administrators.</p>
+    
+    <div v-if="!isAuthenticated">
+      <router-link to="/login" class="btn btn-primary">Login</router-link>
+    </div>
+    
+    <div v-else>
+      <p>Welcome back, {{ currentUser.username }}!</p>
+      <router-link to="/dashboard" class="btn btn-primary">Go to Dashboard</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Home',
+  computed: {
+    ...mapGetters(['isAuthenticated', 'currentUser'])
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  padding: 40px;
+}
+
+.btn {
+  display: inline-block;
+  padding: 10px 20px;
+  margin: 10px;
+  background-color: #42b983;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+}
+
+.btn-primary {
+  background-color: #42b983;
+}
+</style>
