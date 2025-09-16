5c7fccbe2a25dc98a105e5e5098baf1dce7d9185
CNyuchenZhang <1029238540@qq.com>
2025-09-16 17:57:28 +0800

    1

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -43,7 +43,8 @@ export default defineConfig({
     }
   },
   server: {
-    port: 5173,
+    host: '0.0.0.0',
+    port: 8080,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
