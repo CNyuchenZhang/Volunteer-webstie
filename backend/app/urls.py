420322bb8ff939c69feabca3549a9a5d25180170
xiejiani1207 <e1547168@u.nus.edu>
2025-10-29 13:06:35 +0800

    Revert "修改了docker-compose文件和nginx文件，并调整了后端的url。"
    This reverts commit da5ae39315b7d5cfe675442ef4a342ef4eb3d1e3.

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -22,9 +22,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/activity/', include('activity.urls')),
-    path('api/v1/user/', include('user.urls')),
-    path('api/v1/notification/', include('notification.urls')),
+    path('api/v1/', include('activity.urls')),
+    path('api/v1/', include('user.urls')),
+    path('api/v1/', include('notification.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
